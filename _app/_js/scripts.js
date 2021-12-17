104dd02c549de4322b3b6212a7bdd5bebc167dd6
PavelRoR <pavelvp2182@yandex.ru>
2021-12-17 21:09:45 +0500

    Main page is adapted

--- _app/_js/scripts.js
+++ _app/_js/scripts.js
@@ -1,4 +1,13 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    /* Якорь */
+    $(".menu-list li a").click(function (h) {
+        h.preventDefault();
+        var f = $(this).attr("href"),
+            g = $(f).offset().top;
+        $("body,html").animate({
+            scrollTop: g
+        }, 1500)
+    });
     $(function () {
         var check = $('.check', this),
             email = $('.input-mail', this),
@@ -52,7 +61,7 @@ $(document).ready(function() {
     // Модалка
     $('.button-up').magnificPopup({
         type: 'inline',
-        midClick: true, 
+        midClick: true,
         mainClass: 'mfp-fade'
     });
     $('.text-rev-link').magnificPopup({
@@ -76,14 +85,46 @@ $(document).ready(function() {
         centerMode: true,
         prevArrow: '<button type="button" class="slick-prev">‹</button>',
         nextArrow: '<button type="button" class="slick-next">›</button>',
-        responsive: [
-        {
+        responsive: [{
             breakpoint: 768,
             settings: {
                 slidesToShow: 1
             }
-        }
-        ]
+        }]
     });
-/*Конец документа*/
+    // Menu
+    var mobMenu = $('.mobile-menu'),
+        headNav = $('.main-menu'),
+        mml1 = $('.mobile-menu-line-1'),
+        mml2 = $('.mobile-menu-line-2'),
+        mml3 = $('.mobile-menu-line-3'),
+        body = $('body');
+
+    mobMenu.on('click', function () {
+        if (!headNav.hasClass('nav-active')) {
+            $(this).css('position', 'absolute');
+            body.css('overflow', 'hidden');
+            headNav.addClass('nav-active');
+            mml1.addClass('switched');
+            mml2.addClass('switched');
+            mml3.addClass('switched');
+        } else {
+            $(this).css('position', 'absolute');
+            body.css('overflow', 'auto');
+            headNav.removeClass('nav-active');
+            mml1.removeClass('switched');
+            mml2.removeClass('switched');
+            mml3.removeClass('switched');
+        }
+        if ($(body).width() <992){
+            $('.main-menu li a').on('click', function(){
+                body.css('overflow', 'auto');
+                headNav.removeClass('nav-active');
+                mml1.removeClass('switched');
+                mml2.removeClass('switched');
+                mml3.removeClass('switched');
+            });
+        }
+    });
+    /*Конец документа*/
 });
