6169c9d66b15afd12aa79dec63e67df07971a35f
PavelRoR <pavelvp2182@yandex.ru>
2022-01-03 10:50:50 +0500

    Video revs added

--- _app/_js/scripts.js
+++ _app/_js/scripts.js
@@ -92,6 +92,15 @@ $(document).ready(function () {
             }
         }]
     });
+    /*Видео-отзывы*/
+    $(".video-wrapper-rev").click(function () {
+        $(".video-wrapper-rev iframe").each(function () {
+            var l = $(this).parent().attr('data-img');
+            $(this).parent().html('<img src="' + l + '" alt="Видео отзыв"><div class="yt-button"></div>');
+        })
+        var a = $(this).attr("data-youtube");
+        $(this).html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&mute=1&modestbranding=1" allowfullscreen></iframe>')
+    });
     // Menu
     var mobMenu = $('.mobile-menu'),
         headNav = $('.main-menu'),
